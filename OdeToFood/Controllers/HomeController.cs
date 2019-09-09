d6e3743278504785abc0d236149d8b76d8cf3750
clgreenwald <clgreenwald84@gmail.com>
2019-09-08 22:43:26 -0500

    Add project files.

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Chris";
+            model.Location = "Hutchinson, Minnesota";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
